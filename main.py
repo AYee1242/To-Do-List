88be8ecc2c8253077a63f6694ee41003c4133e11
AYee1242 <alexanderyee_300@outlook.com>
2022-01-11 14:35:23 -0500

    update database url

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
 bootstrap = Bootstrap(app)
 
 # Setting up app to work with sqlalchemy
-app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL")
+app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL1")
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
 
